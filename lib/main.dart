f20c29ec7560b99b8a1f5e11a4f92edbbe0de02d
SaeedBoghraei18 <boghraeii>
2025-09-03 11:16:29 +0330

    add profile screen design to project

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:apple_shop/screens/bestseller_screen.dart';
 import 'package:apple_shop/screens/category_screen.dart';
 import 'package:apple_shop/screens/home_screen.dart';
 import 'package:apple_shop/screens/product_detail_screen.dart';
+import 'package:apple_shop/screens/profilescreen.dart';
 import 'package:apple_shop/screens/shoppingbasket_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -21,7 +22,7 @@ class Application extends StatelessWidget {
       splitScreenMode: true,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Product_Detail_Screen(),
+        home: Profile_Screen(),
       ),
     );
   }
